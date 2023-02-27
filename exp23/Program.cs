c8155e028ee746388ca4c0362ec9f89dc213289e
Stas <caplan_74@mail.ru>
2023-02-28 00:14:36 +0500

    ex23

---
+++ exp23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+int number = ReadInt("Введите число  ");
+for (int i = 1; i <= number; i++ )
+{
+    Console.Write($"{i*i*i} |");
+} 
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
