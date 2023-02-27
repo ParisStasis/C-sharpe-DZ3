cf25922d4b6c0b148b8345078190aa45e1b28083
Stas <caplan_74@mail.ru>
2023-02-27 23:40:56 +0500

    ex19

---
+++ exp19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19 Программа принимает число и проверяет является ли оно палиндромом.
+
+
+Console.WriteLine("ВВедите число  ");
+string number = Console.ReadLine();
+int length = number.Length;
+if ( length == 5 )
+{
+    if (number[0]==number[4]&& number[1]==number[3])
+    {
+        Console.WriteLine($"{number} - является палиндромом" );
+    
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не является палиндромом");
+    }
+}
